2b0ffe6accde3442711da5245fb5eef2c5cd1ec0
Charis Mavrotsoupakis <harmavr@gmail.com>
2024-03-08 13:27:28 +0200

    UI change

--- src/components/Programm.jsx
+++ src/components/Programm.jsx
@@ -16,9 +16,11 @@ export default function Programm({ value }) {
 
   return (
     <Modal className="programm" open={value}>
-      <h2>Today's Programm</h2>
+      <h2>
+        Today's Programm: <span>{programm}</span>
+      </h2>
       <h3>{coach}</h3>
-      <h3>{programm}</h3>
+
       <h3>
         <img style={{ width: "500px" }} src={coachImg} alt="" />
       </h3>
