23c06221b766c3d0b222d76691760add30c7f308
Charis Mavrotsoupakis <harmavr@gmail.com>
2024-03-08 12:10:29 +0200

    add a pop up window that shows the programm of the day clicked

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,15 @@
-export default function Modal() {
-  return <dialog></dialog>;
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+export default function Modal({ children, open }) {
+  const dialog = useRef();
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    }
+  }, [open]);
+  return createPortal(
+    <dialog ref={dialog}>{children}</dialog>,
+    document.getElementById("modal")
+  );
 }
