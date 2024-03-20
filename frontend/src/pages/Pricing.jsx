8782d4f35646d790d9a518363c4c66508aafb9d8
Charis Mavrotsoupakis <harmavr@gmail.com>
2024-03-20 11:59:17 +0200

    dummy text for pricing sector

--- frontend/src/pages/Pricing.jsx
+++ frontend/src/pages/Pricing.jsx
@@ -1,3 +1,44 @@
 export default function Pricing() {
-  return <h1>Pricing</h1>;
+  return (
+    <>
+      <h1>Pricing</h1>
+      <p>Select your plan</p>
+      <table>
+        <tr>
+          <th>Basic Plan</th>
+          <th>Weekly Plan</th>
+          <th>Monthly Plan</th>
+        </tr>
+        <tr>
+          <td>12</td>
+          <td>20</td>
+          <td>30</td>
+        </tr>
+        <tr>
+          <td>
+            <ul>
+              <li>Smart workout plan</li>
+              <li>At home workouts</li>
+            </ul>
+          </td>
+          <td>
+            <ul>
+              <li>Pro gyms</li>
+              <li>Smart workout plans</li>
+              <li>At home workouts</li>
+            </ul>
+          </td>
+          <td>
+            <ul>
+              <li>ELITE gyms and classes</li>
+              <li>Pro gyms</li>
+              <li>Smart workout plans</li>
+              <li>At home workouts</li>
+              <li>Personal Trainings</li>
+            </ul>
+          </td>
+        </tr>
+      </table>
+    </>
+  );
 }
