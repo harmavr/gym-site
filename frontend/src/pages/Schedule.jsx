825e997238e6d87f6aae168eeb23b76d9c6c18b7
Charis Mavrotsoupakis <harmavr@gmail.com>
2024-03-21 17:19:47 +0200

    remove warning for correct dom elements

--- frontend/src/pages/Schedule.jsx
+++ frontend/src/pages/Schedule.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import classes from "./Schedule.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { showProgramm } from "../redux/actions";
 import Programm from "../components/Programm";
@@ -16,8 +15,8 @@ export default function Schedule() {
 
   const [value, onChange] = useState(new Date());
   return (
-    <div class="flex flex-col items-center justify-center relative h-full">
-      <h1 class="text-3xl font-bold text-gray-800 mb-4">Schedule</h1>
+    <div className="flex flex-col items-center justify-center relative h-full">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">Schedule</h1>
       <Calendar
         className="absolute top-full bg-white border border-gray-300 shadow-md"
         onChange={onChange}
