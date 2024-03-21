1f0b9fc4be886dbd60c20defc1239b7320f54fd5
Charis Mavrotsoupakis <harmavr@gmail.com>
2024-03-21 17:37:47 +0200

    add background image to the homePage and make some dummy styling changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,17 @@
+import homePageImg from "../assets/home-page.avif";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center mt-8 mb-16">
+    <div
+      className="flex flex-col items-center mt-8 mb-16 bg-cover bg-center h-screen"
+      style={{ backgroundImage: `url(${homePageImg})` }}
+    >
       <div className="mt-16 bg-gray-700 rounded-full p-5">
         <span>Awsome fitness club</span>
       </div>
       <div className="flex flex-col gap-4 text-2xl overflow-visible p-5 m-5 bg-yellow-600 rounded-full">
         <span>
-          Make the <span>PERFECT </span>body
+          Make the <span className="text-green-700">PERFECT </span>body
         </span>
       </div>
     </div>
